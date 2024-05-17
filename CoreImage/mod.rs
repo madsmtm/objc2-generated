93bf4497fa8ee0a3cde54ea4ae2fbb2cf45ed8e1
Mads Marquart <mads@marquart.dk>
2024-05-17 03:40:45 +0200

    Fix compiling for GNUStep

--- CoreImage/mod.rs
+++ CoreImage/mod.rs
@@ -13,7 +13,7 @@
 #![allow(clippy::identity_op)]
 #![allow(clippy::missing_safety_doc)]
 
-#[link(name = "CoreImage", kind = "framework")]
+#[cfg_attr(feature = "apple", link(name = "CoreImage", kind = "framework"))]
 extern "C" {}
 
 #[cfg(feature = "CIBarcodeDescriptor")]
